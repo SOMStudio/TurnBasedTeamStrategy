02e6466150a71ca7d7b678382f58c811f764e064
sergii.momot <srg.momot@gmail.com>
2022-12-27 08:17:45 +0200

    Step manager select logic

--- Assets/Scripts/View/StepManager.cs
+++ Assets/Scripts/View/StepManager.cs
@@ -9,11 +9,20 @@ namespace View
         [Header("Base")]
         [SerializeField] private int _x;
         [SerializeField] private int _y;
+        
+        [Header("Main")]
+        [SerializeField] private Transform _mainMesh;
+        [SerializeField] private float _pointerEnterScale = 1.2f; 
 
         public Vector2Int PlaceStep => new Vector2Int(_x, _y);
 
         public event Action<int, int> OnOverStepEvent;
         public event Action<int, int> OnClickStepEvent;
+
+        private void SetPointerEnterState()
+        {
+            
+        }
         
         public void OnPointerEnter(PointerEventData eventData)
         {
